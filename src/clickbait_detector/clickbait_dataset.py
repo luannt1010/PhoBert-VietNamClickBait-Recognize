e32f2b15dd004af7d01a4bc329895526c456ecac
luannt1010 <luanntai13@gmail.com>
2026-06-24 22:37:16 +0700

    add code create dataset

---
+++ src/clickbait_detector/clickbait_dataset.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from src.clickbait_detector.preprocessing import prepare_data
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+
+class ClickBaitDataset(Dataset):
+    def __init__(self, root_dir, max_len=50):
+        super().__init__()
+
+        self.df = pd.read_csv(root_dir)
+        self.labels = self.df["final_label"].tolist()
+        self.headlines = self.df["title"].tolist()
+        self.tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base-v2")
+        self.input_ids, self.attention_mask = prepare_data(self.headlines, self.tokenizer, max_len)
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        target = self.labels[idx]
+        input_ids = self.input_ids[idx]
+        attention_mask = self.attention_mask[idx]
+        return target, input_ids, attention_mask
+
+# if __name__ == "__main__":
+#     import pandas as pd
+#     from transformers import AutoTokenizer
+#     tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base-v2")
+#     train_df = pd.read_csv(r"D:\private\clickbait_detect_proj\data\raw\train_clickbait.csv")
+#     d = ClickBaitDataset(train_df, tokenizer)
+#     print("1")
+#
+#     print(d[1])
