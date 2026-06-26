9644f5d4835aab46af20abf6ac01c62a7cb4b78d
luannt1010 <luanntai13@gmail.com>
2026-06-26 13:28:02 +0700

    add update code train

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
-import torch.nn as nn
 import torch
 import argparse
+import pandas as pd
+import torch.nn as nn
 from clickbait_detector import create_dataloader, train, ClickBaitDataset, Model
 
 def get_args():
@@ -18,6 +19,8 @@ def get_args():
     return parser.parse_args()
 
 if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     args = get_args()
     train_path = args.train_path
     val_path = args.val_path
@@ -25,9 +28,13 @@ if __name__ == "__main__":
     max_len = args.max_len
     batch_size = args.batch_size
 
-    train_dataset = ClickBaitDataset(train_path, max_len)
-    val_dataset = ClickBaitDataset(val_path, max_len)
-    test_dataset = ClickBaitDataset(test_path, max_len)
+    train_df = pd.read_csv(train_path)
+    val_df = pd.read_csv(val_path)
+    test_df = pd.read_csv(test_path)
+
+    train_dataset = ClickBaitDataset(train_df, max_len)
+    val_dataset = ClickBaitDataset(val_df, max_len)
+    test_dataset = ClickBaitDataset(test_df, max_len)
     print(f"Length of train dataset: {len(train_dataset)}")
     print(f"Length of validation dataset: {len(val_dataset)}")
     print(f"Length of test dataset: {len(test_dataset)}")
@@ -37,6 +44,8 @@ if __name__ == "__main__":
     model = Model()
 
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    loss_fn = nn.BCEWithLogitsLoss()
+    train_label_counts = train_df["final_label"].value_counts()
+    pos_weight = torch.tensor([train_label_counts[0]/train_label_counts[1]], dtype=torch.float32).to(device)
+    loss_fn = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
 
     train(model, train_loader, val_loader, loss_fn, optimizer, args.epochs, args.save_path)
