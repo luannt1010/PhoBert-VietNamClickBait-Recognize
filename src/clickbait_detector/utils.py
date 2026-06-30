04f99998f6e4f528c65ccbc3ed9d53d6a9abce94
luannt1010 <luanntai13@gmail.com>
2026-06-30 19:50:54 +0700

    add update code util

--- src/clickbait_detector/utils.py
+++ src/clickbait_detector/utils.py
@@ -19,14 +19,14 @@ def create_dataloader(train_dataset, val_dataset, test_dataset, batch_size=8):
     test_loader = DataLoader(test_dataset, batch_size, shuffle=False)
     return train_loader, val_loader, test_loader
 
-def train_one_epoch(model, train_pbar, optimizer, loss_fn, device, threshold):
+def train_one_epoch(model, train_pbar, optimizer, loss_fn, device):
     model.train()
     train_running_loss = 0.0
     num_corrects = 0.0
     num_preds = 0.0
     all_preds = []
     all_gts = []
-
+    thresholds = [0.2, 0.4, 0.6, 0.8]
     for target, input_ids, attention_mask in train_pbar:
         target = target.to(device)
         input_ids = input_ids.to(device)
@@ -40,27 +40,38 @@ def train_one_epoch(model, train_pbar, optimizer, loss_fn, device, threshold):
         optimizer.step()
 
         logits = torch.sigmoid(outputs).squeeze(1)
-        preds = (logits >= threshold).to(torch.int64)
-        num_corrects += (preds == target).sum().item()
+        preds_50 = (logits >= 0.5).to(torch.int64)
+        for threshold in thresholds:
+            preds = (logits >= threshold).to(torch.int64)
+            all_preds.append(preds.tolist())
+            all_gts.append(target.tolist())
+
+        num_corrects += (preds_50 == target).sum().item()
         num_preds += target.size(0)
 
         train_running_loss += loss.item()
 
-        all_preds.extend(preds.tolist())
-        all_gts.extend(target.tolist())
+
     train_epoch_loss = train_running_loss / len(train_pbar)
     train_epoch_acc = num_corrects / num_preds
-    precision = precision_score(all_gts, all_preds, average="macro", zero_division=0.0)
-    recall = recall_score(all_gts, all_preds, average="macro", zero_division=0.0)
-    return train_epoch_loss, train_epoch_acc, precision, recall
+    p, r = compute_p_r_threshold(all_preds, all_gts)
+    return train_epoch_loss, train_epoch_acc, p, r
 
-def evaluate(model, val_pbar, loss_fn, device, threshold):
+def compute_p_r_threshold(all_preds, all_gts):
+    p, r = 0, 0
+    num_preds = len(all_preds)
+    for i in range(num_preds):
+        p += precision_score(all_gts[i], all_preds[i], average="macro", zero_division=0)
+        r += recall_score(all_gts[i], all_preds[i], average="macro", zero_division=0)
+    return p / (num_preds + 1e-6), r / (num_preds + 1e-6)
+
+def evaluate(model, val_pbar, loss_fn, device):
     model.eval()
     val_running_loss = 0.0
     num_corrects = 0.0
     num_preds = 0.0
-    all_preds = []
-    all_gts = []
+    all_preds, all_gts = [], []
+    thresholds = [0.2, 0.4, 0.6, 0.8]
     with torch.no_grad():
         for target, input_ids, attention_mask in val_pbar:
             target = target.to(device)
@@ -71,21 +82,23 @@ def evaluate(model, val_pbar, loss_fn, device, threshold):
             loss = loss_fn(outputs, target.unsqueeze(1).float())
 
             logits = torch.sigmoid(outputs).squeeze(1)
-            preds = (logits >= threshold).to(torch.int64)
-            num_corrects += (preds == target).sum().item()
-            num_preds += target.size(0)
+            preds_50 = (logits >= 0.5).to(torch.int64)
+            for threshold in thresholds:
+                preds = (logits >= threshold).to(torch.int64)
+                all_preds.append(preds.tolist())
+                all_gts.append(target.tolist())
 
+            num_corrects += (preds_50 == target).sum().item()
+            num_preds += target.size(0)
             val_running_loss += loss.item()
 
-            all_preds.extend(preds.tolist())
-            all_gts.extend(target.tolist())
+
         val_epoch_loss = val_running_loss / len(val_pbar)
         val_epoch_acc = num_corrects / num_preds
-        precision = precision_score(all_gts, all_preds, average="macro", zero_division=0.0)
-        recall = recall_score(all_gts, all_preds, average="macro", zero_division=0.0)
-    return val_epoch_loss, val_epoch_acc, precision, recall
+        p, r = compute_p_r_threshold(all_preds, all_gts)
+    return val_epoch_loss, val_epoch_acc, p, r
 
-def train(model, train_loader, val_loader, loss_fn, optimizer, num_epochs, sp, scheduler=None, threshold=0.5):
+def train(model, train_loader, val_loader, loss_fn, optimizer, num_epochs, sp, scheduler=None):
 
     total_time = 0.0
     best_val_acc = 0.0
@@ -109,9 +122,9 @@ def train(model, train_loader, val_loader, loss_fn, optimizer, num_epochs, sp, s
 
         train_pbar = tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs} [Training]", leave=False)
         train_epoch_loss, train_epoch_acc, tr_precision, tr_recall = train_one_epoch(model, train_pbar, optimizer,
-                                                                               loss_fn, device, threshold)
+                                                                               loss_fn, device)
         val_pbar = tqdm(val_loader, desc=f"Epoch {epoch+1}/{num_epochs} [Validating]", leave=False)
-        val_epoch_loss, val_epoch_acc, val_precision, val_recall = evaluate(model, val_pbar, loss_fn, device, threshold)
+        val_epoch_loss, val_epoch_acc, val_precision, val_recall = evaluate(model, val_pbar, loss_fn, device)
 
         history["train_loss"].append(train_epoch_loss)
         history["val_loss"].append(val_epoch_loss)
