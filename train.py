6fba8a1a735f82d788d3e66c521f0ed86009af7a
luannt1010 <luanntai13@gmail.com>
2026-07-01 23:15:11 +0700

    add update trainer

--- train.py
+++ train.py
@@ -7,18 +7,19 @@ from clickbait_detector import create_dataloader, train, ClickBaitDataset, Model
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--train_path", type=str, default=r"./data/raw/train_clickbait.csv")
-    parser.add_argument("--val_path", type=str, default=r"./data/raw/val_clickbait.csv")
-    parser.add_argument("--test_path", type=str, default=r"./data/raw/test_clickbait.csv")
+    parser.add_argument("--train_path", type=str, default=r"./data/processed/base_train.csv")
+    parser.add_argument("--val_path", type=str, default=r"./data/processed/base_val.csv")
+    parser.add_argument("--test_path", type=str, default=r"./data/processed/base_test.csv")
     parser.add_argument("--save_path", type=str, default=r"./artifacts")
 
-    parser.add_argument("--batch_size", type=int, default=8)
-    parser.add_argument("--lr", type=float, default=5e-4)
-    parser.add_argument("--epochs", type=int, default=1)
-    parser.add_argument("--max_len", type=int, default=50)
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--epochs", type=int, default=30)
+    parser.add_argument("--max_len", type=int, default=256)
+    parser.add_argument("--patience", type=int, default=0)
     return parser.parse_args()
 
-if __name__ == "__main__":
+def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     args = get_args()
@@ -27,6 +28,7 @@ if __name__ == "__main__":
     test_path = args.test_path
     max_len = args.max_len
     batch_size = args.batch_size
+    patience = args.patience
 
     train_df = pd.read_csv(train_path)
     val_df = pd.read_csv(val_path)
@@ -43,9 +45,17 @@ if __name__ == "__main__":
 
     model = Model()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    train_label_counts = train_df["final_label"].value_counts()
-    pos_weight = torch.tensor([train_label_counts[0]/train_label_counts[1]], dtype=torch.float32).to(device)
+    optimizer = torch.optim.AdamW([{"params": model.bert.parameters(), "lr": 5e-6},
+                                   {"params": model.classify1.parameters(), "lr": 2e-5},
+                                   {"params": model.classify2.parameters(), "lr": 2e-5},
+                                   {"params": model.classify3.parameters(), "lr": 2e-5},
+                                   {"params": model.classify4.parameters(), "lr": 2e-5}], weight_decay=0.01)
+    train_label_counts = train_df["label"].value_counts()
+    pos_weight = torch.tensor([train_label_counts[0] / train_label_counts[1]], dtype=torch.float32).to(device)
     loss_fn = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
 
-    train(model, train_loader, val_loader, loss_fn, optimizer, args.epochs, args.save_path)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="max")
+    train(model, train_loader, val_loader, loss_fn, optimizer, args.epochs, args.save_path, scheduler, None if patience==0 else patience)
+
+if __name__ == "__main__":
+    main()
