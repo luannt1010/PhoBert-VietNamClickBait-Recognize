5d8bf77b04de62da1b247f2beddf71e6c6a0d1c6
luannt1010 <luanntai13@gmail.com>
2026-07-01 23:10:28 +0700

    add update model

--- src/clickbait_detector/net.py
+++ src/clickbait_detector/net.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 from transformers import AutoModel
 
@@ -6,41 +5,46 @@ class Model(nn.Module):
     def __init__(self, dropout_rate=0.3):
         super().__init__()
         self.bert = AutoModel.from_pretrained("vinai/phobert-base-v2")
+
         self.dropout = nn.Dropout(dropout_rate)
-        self.classifier = nn.Linear(self.bert.config.hidden_size, 1)
-        nn.init.normal_(self.classifier.weight,mean=0.0, std=0.02)
-        nn.init.zeros_(self.classifier.bias)
+
+        self.classify1 = nn.Sequential(nn.Linear(self.bert.config.hidden_size, 256, bias=False),
+                                        nn.BatchNorm1d(256),
+                                        nn.ReLU(),
+                                       nn.Dropout(0.1))
+
+        self.classify2 = nn.Sequential(nn.Linear(256, 128, bias=False),
+                                       nn.BatchNorm1d(128),
+                                       nn.ReLU(),
+                                       nn.Dropout(0.1))
+
+        self.classify3 = nn.Sequential(nn.Linear(128, 64, bias=False),
+                                        nn.BatchNorm1d(64),
+                                        nn.ReLU(),
+                                       nn.Dropout(0.1))
+        self.classify4 = nn.Sequential(nn.Linear(64, 1, bias=False),
+                                       nn.BatchNorm1d(1))
+
+        self._init_weight()
+
+    def _init_weight(self):
+        for module in [self.classify1, self.classify2, self.classify3, self.classify4]:
+            for m in module.modules():
+                if isinstance(m, nn.Linear):
+                    nn.init.normal_(m.weight, mean=0.0, std=0.02)
+
 
     def forward(self, input_ids, attention_mask):
         last_hidden_state, output = self.bert(
             input_ids=input_ids,
             attention_mask=attention_mask,
-            return_dict=False  # Dropout will errors if without this
-        )
-        out = self.dropout(output)
-        out = self.classifier(out)
+            return_dict=False)  # Dropout will errors if without this
+
+        output = self.dropout(output)
+        output = self.classify1(output)
+        output = self.classify2(output)
+        output = self.classify3(output)
+        out = self.classify4(output)
         return out
 
-# if __name__ == "__main__":
-#     model = Model()
-#     print(1)
-#     from clickbait_dataset import ClickBaitDataset
-#     from transformers import AutoTokenizer
-#     from torch.utils.data import DataLoader
-#     tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base-v2")
-#     d = ClickBaitDataset(r"D:\private\clickbait_detect_proj\data\raw\train_clickbait.csv")
-#     d = DataLoader(d, batch_size=16, shuffle=True)
-#     for target, input_ids, attention_mask in d:
-#         print(input_ids.shape)
-#         outputs = torch.sigmoid(model(input_ids, attention_mask))
-#         print("Outputs:", outputs.squeeze(1))
-#
-#         logits = outputs.squeeze(1)
-#         preds = (logits >= 0.5).to(torch.int64)
-#
-#
-#         num_correct = (preds==target).sum().item()
-#         print(preds)
-#         print(target)
-#         print(num_correct)
-#         break
+
