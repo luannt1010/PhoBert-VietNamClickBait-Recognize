95ae2d08309f96acd7a86251d00271b0c6418951
luannt1010 <luanntai13@gmail.com>
2026-07-05 22:32:08 +0700

    add update code data

--- src/clickbait_detector/clickbait_dataset.py
+++ src/clickbait_detector/clickbait_dataset.py
@@ -1,15 +1,15 @@
-from clickbait_detector.preprocessing import prepare_data
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer
+from clickbait_detector.preprocessing import prepare_data
 
 class ClickBaitDataset(Dataset):
-    def __init__(self, df, max_len=50):
+    def __init__(self, df, max_len=50, tokenizer_dir="vinai/phobert-base-v2"):
         super().__init__()
 
         self.df = df
         self.labels = self.df["label"].tolist()
         self.headlines = self.df["title_combined"].tolist()
-        self.tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base-v2")
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir)
         self.input_ids, self.attention_mask = prepare_data(self.headlines, self.tokenizer, max_len)
 
     def __len__(self):
